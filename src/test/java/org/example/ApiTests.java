ac1a3b04d485542b331145c95af8dee11271d6c0
anna <simankovaana>
2024-10-07 18:16:02 +0300

    first commit

---
+++ src/test/java/org/example/ApiTests.java
@@ -0,0 +1,27 @@
+package org.example;
+
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ApiTests {
+
+    static {
+        RestAssured.baseURI="https://jsonplaceholder.typicode.com";
+    }
+
+    @Test
+    public void testGetRequest (){
+        given()
+                .when()
+                .get("/posts/1")
+                .then()
+                .statusCode(200)
+                .body("userId", equalTo(1))
+                .body("id", equalTo(1))
+                .body("title", equalTo("sunt aut facere repellat provident occaecati excepturi optio reprehenderit"));
+    }
+}
